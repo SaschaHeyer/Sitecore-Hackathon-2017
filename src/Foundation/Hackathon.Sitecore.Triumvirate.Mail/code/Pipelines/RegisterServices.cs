e93a1a809a9629e12470b5134a38d94dc1ae7f73
Marina Gluzberg <marina.gluzberg@ecx.io>
2017-03-04 09:29:36 +0100

    Added DI to services

---
+++ src/Foundation/Hackathon.Sitecore.Triumvirate.Mail/code/Pipelines/RegisterServices.cs
@@ -0,0 +1,26 @@
+﻿using Hackathon.Sitecore.Triumvirate.Foundation.Mail.Services;
+using Hackathon.Sitecore.Triumvirate.Foundation.Mail.Services.Imp;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.XA.Foundation.IOC.Pipelines.IOC;
+
+namespace Hackathon.Sitecore.Triumvirate.Foundation.Mail.Pipelines
+{
+    /// <summary>
+    /// RegisterServices
+    /// </summary>
+    /// <author>
+    /// M. Gluzberg, Mrz-2017
+    /// </author>
+    public class RegisterServices : IocProcessor
+    {
+        /// <summary>
+        /// Process
+        /// </summary>
+        /// <param name="args">args</param>
+        public override void Process(IocArgs args)
+        {
+            args.ServiceCollection.AddTransient<IMailSendService, MailSendService>();
+            args.ServiceCollection.AddTransient<IFormSendService, FormSendService>();
+        }
+    }
+}
