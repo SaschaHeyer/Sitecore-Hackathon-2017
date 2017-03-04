a407dbfaf7d00ee1b1488c3f440788ebf5ba3baf
Christian <christian.hahn@de.ibm.com>
2017-03-04 08:50:27 +0100

    - added various stuf

--- src/Feature/Hackathon.Sitecore.Triumvirate.Form/code/Controllers/Container/FormContainerController.cs
+++ src/Feature/Hackathon.Sitecore.Triumvirate.Form/code/Controllers/Container/FormContainerController.cs
@@ -1,14 +1,35 @@
 ﻿using System.Web.Mvc;
 using Sitecore.XA.Foundation.Mvc.Controllers;
+using Sitecore.XA.Foundation.Mvc.Repositories.Base;
 
 namespace Hackathon.Sitecore.Triumvirate.Feature.Form.Controllers.Container
 {
     public class FormContainerController : StandardController
     {
+        /// <summary>
+        /// Standard Sxa Model Repository
+        /// </summary>
+        public IModelRepository _repository;
+
+        /// <summary>
+        /// c'tor
+        /// </summary>
+        /// <param name="modelRepository">Reference to Model Repository</param>
+        protected FormContainerController(IModelRepository modelRepository)
+        {
+            _repository = modelRepository;
+        }
+
         // GET: FormContainer
         public override ActionResult Index()
         {
-            return View();
+            return View("Index", this.GetModel());
+        }
+
+        [HttpPost]
+        public void Submit(string parameter)
+        {
+            
         }
     }
 }
